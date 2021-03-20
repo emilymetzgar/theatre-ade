7fc301c38ecce53dd0fb34fbc309a6fc6feb66fe
Darren Watson <v_tec@live.com>
2021-03-20 10:14:54 -0500

    ...

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,10 @@
-
+const URL = 'https://aztro.sameerkumar.website/?sign=aries&day=today';
+fetch(URL, {
+    method: 'POST'
+})
+.then(response => response.json())
+.then(json => {
+    const sign = json.current_sign;
+    console.log(sign);
+});
 
